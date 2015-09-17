d032d0342a11c8a9a50bfd8a32c416f23057296c
Becky Skillings <beckyskillings@gmail.com>
2015-09-17 12:51:27 -0600

    hour and day of week count working

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require "csv"
 require "sunlight/congress"
 require "erb"
+require "date"
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -18,8 +19,7 @@ def clean_phone_number(phone)
 				clean_phone.push (number)
 			end
 		end
-		if  clean_phone.length == 11 #having trouble getting it to act right if the first of 11 digits is 1
-			#check if first is 1 and delete if it is, or return nil if it isn't
+		if  clean_phone.length == 11 
 			
 			if clean_phone[0] == "1"
 				clean_phone.shift
@@ -50,14 +50,38 @@ def save_thank_you_letters(id, form_letter)
 	end
 end
 
+def calculate_peak_hour
+	data = open_attendees
+	hours = Array.new(24, 0)
+	days_of_week = Hash.new
+	data.each do |row| #not getting in here
+		parsed_date = DateTime.strptime(row[:regdate], "%m/%d/%y %H:%M")
+		hours[parsed_date.hour.to_i] += 1
+		day =  parsed_date.strftime("%A")
+		unless days_of_week.has_key?(day)
+			days_of_week[day] = 0
+		end
+		days_of_week[day] += 1
+	end
+	hours.each_with_index do |count, index|
+	puts "#{count.to_s} people registered at hour #{index.to_s}"
+	end
+	puts days_of_week
+end
+
+def open_attendees
+	CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+end
+
 puts "EventManager Initialized."
 
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents = open_attendees
 
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
 
 contents.each do |row|
+
 	id = row[0]
 	
 	name = row[:first_name]
@@ -69,8 +93,12 @@ contents.each do |row|
 	legislators = legislators_by_zipcode(zipcode)
 
 	form_letter = erb_template.result(binding)
+
+		
 	
 	#save_thank_you_letters(id, form_letter)
-	puts "#{name}, #{home_phone}"
+	puts "#{name}, #{home_phone}" 
 
 end
+
+calculate_peak_hour
